3f55a6b67df7ca9db2532793dcdd586f5c1433c9
Janilson Varela <janilsonjvs@gmail.com>
2024-08-23 22:36:46 -0300

    feat: views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from core.models import Contato, Formapagamento, Conta
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from core.serializers import (
+    ContatoSerializer,
+    FormapagamentoSerializer,
+    ContaSerializer,
+)
+from core.permissions import (
+    ContatoPermission,
+    FormapagamentoPermission,
+    ContaPermission,
+)
 
-# Create your views here.
+from core.filters import ContatoFilter, FormapagamentoFilter, ContaFilter
+
+
+class ContatoViewSet(ModelViewSet):
+    queryset = Contato.objects.all()
+    serializer_class = ContatoSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser, ContatoPermission]
+    filter_class = ContatoFilter
+
+
+class FormapagamentoViewSet(ModelViewSet):
+    queryset = Formapagamento.objects.all()
+    serializer_class = FormapagamentoSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser, FormapagamentoPermission]
+    filter_class = FormapagamentoFilter
+
+
+class ContaViewSet(ModelViewSet):
+    queryset = Conta.objects.all()
+    serializer_class = ContaSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser, ContaPermission]
+    filter_class = ContaFilter
