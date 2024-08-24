9ef2fa31438f27ebac33db00502c2a4715b00813
Janilson Varela <janilsonjvs@gmail.com>
2024-08-23 22:36:29 -0300

    feat: permissions

---
+++ core/permissions.py
@@ -0,0 +1,43 @@
+from rest_framework.permissions import BasePermission
+
+
+class ContatoPermission(BasePermission):
+    def has_permission(self, request, view):
+        if view.action == "create":
+            return request.user.has_perm("core.add_contato")
+        elif view.action == ["list", "retrieve"]:
+            return request.user.has_perm("core.view_contato")
+        elif view.action in ["update", "partial_update"]:
+            return request.user.has_perm("core.change_contato")
+        elif view.action == "destroy":
+            return request.user.has_perm("core.delete_contato")
+        else:
+            return False
+
+
+class FormapagamentoPermission(BasePermission):
+    def has_permission(self, request, view):
+        if view.action == "create":
+            return request.user.has_perm("core.add_formapagamento")
+        elif view.action in ["list", "retrieve"]:
+            return request.user.has_perm("core.view_formapagamento")
+        elif view.action in ["update", "partial_update"]:
+            return request.user.has_perm("core.change_formapagamento")
+        elif view.action == "destroy":
+            return request.user.has_perm("core.delete_formapagamento")
+        else:
+            return False
+
+
+class ContaPermission(BasePermission):
+    def has_permission(self, request, view):
+        if view.action == "create":
+            return request.user.has_perm("core.add_conta")
+        elif view.action in ["list", "retrieve"]:
+            return request.user.has_perm("core.view_conta")
+        elif view.action in ["update", "partial_update"]:
+            return request.user.has_perm("core.change_conta")
+        elif view.action == "destroy":
+            return request.user.has_perm("core.delete_conta")
+        else:
+            return False
