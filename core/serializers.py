41205a2b7f1f2f63a6d11a125ea4979e59bac656
Janilson Varela <janilsonjvs@gmail.com>
2024-08-23 23:08:59 -0300

    feat: serializer base

--- core/serializers.py
+++ core/serializers.py
@@ -2,19 +2,35 @@ from rest_framework import serializers
 from core.models import Contato, Formapagamento, Conta
 
 
-class ContatoSerializer(serializers.ModelSerializer):
+class BaseSerializer(serializers.ModelSerializer):
+
+    def save(self, **kwargs):
+        kwargs["user"] = self.context["request"].user
+        return super().save(**kwargs)
+
+
+class ContatoSerializer(BaseSerializer):
     class Meta:
         model = Contato
         fields = "__all__"
+        extra_kwargs = {
+            "user": {"read_only": True},
+        }
 
 
-class FormapagamentoSerializer(serializers.ModelSerializer):
+class FormapagamentoSerializer(BaseSerializer):
     class Meta:
         model = Formapagamento
         fields = "__all__"
+        extra_kwargs = {
+            "user": {"read_only": True},
+        }
 
 
-class ContaSerializer(serializers.ModelSerializer):
+class ContaSerializer(BaseSerializer):
     class Meta:
         model = Conta
         fields = "__all__"
+        extra_kwargs = {
+            "user": {"read_only": True},
+        }
