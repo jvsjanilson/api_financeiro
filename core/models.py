b080959ab1d4597a88e8dc618c3b57a1f838cbfe
Janilson Varela <janilsonjvs@gmail.com>
2024-08-23 23:08:28 -0300

    feat: add user ao models

--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from core.choices import EstadoChoice
 from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 
 
 class Base(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     ativo = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
 
     class Meta:
         abstract = True
