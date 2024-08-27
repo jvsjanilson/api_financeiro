437073dd43b5df3196e59a1c34439a9179a97300
Janilson Varela <janilsonjvs@gmail.com>
2024-08-27 09:58:11 -0300

    feat: config rotas do swagger

--- setup/urls.py
+++ setup/urls.py
@@ -25,12 +25,30 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.http import HttpResponseRedirect
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 router = DefaultRouter()
 router.register("contatos", ContatoViewSet)
 router.register("formapagamentos", FormapagamentoViewSet)
 router.register("contas", ContaViewSet)
 router.register("recebers", ReceberViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Financeiro API",
+        default_version="v1",
+        description="API para controle financeiro",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="janilsonjvs@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[
+        permissions.AllowAny,
+    ],
+)
 
 
 urlpatterns = [
@@ -39,5 +57,15 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(router.urls)),
     path("auth/", include("rest_framework.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
     path("", lambda request: HttpResponseRedirect("/api/")),
 ]
