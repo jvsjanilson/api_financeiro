c4d72b940cec865d4acc36bbe37081a2ed33071b
Janilson Varela <janilsonjvs@gmail.com>
2024-08-31 20:29:38 -0300

    feat: new fields

--- financeiro/serializers.py
+++ financeiro/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework.serializers import ModelSerializer
-from rest_framework import serializers 
+from rest_framework import serializers
 from financeiro.models import Receber
 
 
 class ReceberSerializer(ModelSerializer):
+    conta_numero = serializers.CharField(source="conta.numero_conta", read_only=True)
+    contato_nome = serializers.CharField(source="contato.nome", read_only=True)
+    formapagamento_descricao = serializers.CharField(
+        source="formapagamento.descricao", read_only=True
+    )
+
     class Meta:
         model = Receber
-        fields = "__all__"
+        # fields = "__all__"
+        exclude = ["created_at", "updated_at"]
         extra_kwargs = {
             "user": {"read_only": True},
         }
+
+
+class BaixaReceberSerializer(serializers.Serializer):
+    data_pagamento = serializers.DateField(required=True)
