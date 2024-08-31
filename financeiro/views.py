f2802794a3b2ed0ba203c73dcf569b2ea0d96eb6
Janilson Varela <janilsonjvs@gmail.com>
2024-08-31 20:29:55 -0300

    feat: actions baixar e estornar conta receber

--- financeiro/views.py
+++ financeiro/views.py
@@ -5,6 +5,10 @@ from financeiro.serializers import ReceberSerializer
 from financeiro.permissions import ReceberPermission
 from financeiro.filters import ReceberFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 class ReceberViewSet(BaseUserViewSet):
@@ -14,3 +18,27 @@ class ReceberViewSet(BaseUserViewSet):
     search_fields = ["documento", "contato__nome"]
     filter_order_by = ["documento", "contato__nome"]
     filterset_class = ReceberFilter
+
+    @action(detail=True, methods=["post"], url_path="baixar-receber")
+    def baixar_receber(self, request, pk=None):
+        receber = get_object_or_404(Receber, pk=pk)
+        data_pagamento = request.data.get("data_pagamento")
+
+        if not data_pagamento:
+            return Response(
+                {"detail": "Este campo é obrigatório."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        receber.data_pagamento = data_pagamento
+        receber.status = "P"
+        receber.save()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"], url_path="estornar-receber")
+    def estornar_receber(self, request, pk=None):
+        receber = get_object_or_404(Receber, pk=pk)
+        receber.data_pagamento = None
+        receber.status = "A"
+        receber.save()
+        return Response(status=status.HTTP_200_OK)
