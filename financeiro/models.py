00aca1ac5e0237bd9fafa562dc2848ed75931718
Janilson Varela <janilsonjvs@gmail.com>
2024-08-31 17:58:42 -0300

    feat: add new fields, conta e formapagamento

--- financeiro/models.py
+++ financeiro/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-from core.models import Base, Contato
+from core.models import Base, Contato, Conta, Formapagamento
 from financeiro.choices import SituacaoChoice
+
 
 class Receber(Base):
     documento = models.CharField(max_length=20)
     contato = models.ForeignKey(Contato, on_delete=models.RESTRICT)
+    conta = models.ForeignKey(Conta, on_delete=models.RESTRICT)
+    formapagamento = models.ForeignKey(Formapagamento, on_delete=models.RESTRICT)
     data_emissao = models.DateField()
     data_vencimento = models.DateField()
     data_pagamento = models.DateField(blank=True, null=True)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=1, choices=SituacaoChoice.choices, default=SituacaoChoice.ABERTO)
+    status = models.CharField(
+        max_length=1, choices=SituacaoChoice.choices, default=SituacaoChoice.ABERTO
+    )
     observacao = models.TextField(blank=True, null=True)
 
     def __str__(self):
