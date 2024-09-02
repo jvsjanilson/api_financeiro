cabac91dd8bf966c88279983e379d56d42cc707f
Janilson Varela <janilsonjvs@gmail.com>
2024-09-02 19:09:08 -0300

    feat: ajuste na paginacao

--- setup/settings.py
+++ setup/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 2,
+    "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
