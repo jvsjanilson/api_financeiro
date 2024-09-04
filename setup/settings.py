5639fed1b718ccedad9a8f55fb4078eb2eedf67c
Janilson Varela <janilsonjvs@gmail.com>
2024-09-04 06:44:18 -0300

    feat: removido login por session

--- setup/settings.py
+++ setup/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
